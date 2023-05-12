7f2bccc9eefe2e4aba927208817d1c6ca1c9a624
kaluosifa <17607149859@163.com>
2023-05-12 14:52:28 +0800

    配置在vscode上开发终端字符版本

---
+++ build/src/CMakeFiles/push_box.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/push_box.dir/main.c.obj"
+  "CMakeFiles/push_box.dir/main.c.obj.d"
+  "libpush_box.dll.a"
+  "push_box.exe"
+  "push_box.exe.manifest"
+  "push_box.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/push_box.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
